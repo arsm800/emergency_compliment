0e95061b8a51d91bc9b0b253155b76a9d1f3b27c
Andrew Smith-Mui <arsm800@gmail.com>
2016-03-30 15:21:35 -0400

    PRs and Merging

---
+++ js/app.js
@@ -0,0 +1,11 @@
+"use strict";
+
+(function() {
+  angular
+  .module("compliments", [])
+  .controller("complimentsController", complimentsController)
+
+  function complimentsController() {
+    var vm = this;
+  }
+})
